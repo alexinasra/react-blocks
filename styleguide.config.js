937f84424ed008a183deabb47e5ed25c23759e03
Alexi Nasra <alexinasra@gmail.com>
2018-04-11 13:18:48 +0300

    configured styleguide config to styles

--- styleguide.config.js
+++ styleguide.config.js
@@ -3,5 +3,8 @@ const path = require('path');
 module.exports = {
   components: 'src/components/**/*.jsx',
   template: path.join(__dirname, 'styleguide/index.html'),
+  require: [
+      path.join(__dirname, './src/react-blocks.scss')
+  ],
   ignore: ['**/index.jsx']
 };
