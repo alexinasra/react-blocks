3fefe5d71302c4a6d1cda368f6ee0d80ea6af9f2
Alexi Nasra <alexinasra@gmail.com>
2018-04-12 15:33:03 +0300

    styleguide template include material icons

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,7 +2,17 @@ const path = require('path');
 
 module.exports = {
   components: 'src/components/**/*.jsx',
-  styleguideDir: path.join(__dirname, 'styleguide'),
+  template: {
+    head: {
+      links: [
+        {
+          rel: 'stylesheet',
+          type: 'text/css',
+          href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
+        }
+      ]
+    }
+  },
   require: [
       path.join(__dirname, './src/react-blocks.scss')
   ],
