7a5df9e8dbef84f47e027fc5e3b46aca58530e1b
Alexi Nasra <alexinasra@gmail.com>
2018-04-15 01:51:51 +0300

    removed raised prop

--- src/components/Button/ButtonGroup.jsx
+++ src/components/Button/ButtonGroup.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-const ButtonGroup = ({ raised, className, ...props }) => (
-  <div className={classnames('button_group', className, { raised })}>
+const ButtonGroup = ({ className, ...props }) => (
+  <div className={classnames('button_group', className)}>
     {props.children}
   </div>
 );
 
 ButtonGroup.propTypes = {
   children: PropTypes.node.isRequired,
-  className: PropTypes.string,
-  raised: PropTypes.bool
+  className: PropTypes.string
 };
 ButtonGroup.defaultProps = {
-  className: undefined,
-  raised: false
+  className: undefined
 };
 
 export default ButtonGroup;
