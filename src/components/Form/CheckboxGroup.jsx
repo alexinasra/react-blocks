3c7d3aa773e5c702e35de487af89ab2ad0134e48
Alexi Nasra <alexinasra@gmail.com>
2018-04-15 04:03:37 +0300

    removed labeledBy prop

--- src/components/Form/CheckboxGroup.jsx
+++ src/components/Form/CheckboxGroup.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-const CheckboxGroup = ({ labelledBy, className, ...props }) => (
-  <div className={classnames('checkbox_group', className)} role="group" aria-labelledby={labelledBy}>
+const CheckboxGroup = ({ className, ...props }) => (
+  <div className={classnames('checkbox_group', className)} role="group">
     {props.children}
   </div>
 );
 
 CheckboxGroup.propTypes = {
-  labelledBy: PropTypes.string.isRequired,
   className: PropTypes.string,
   children: PropTypes.node.isRequired
 };
