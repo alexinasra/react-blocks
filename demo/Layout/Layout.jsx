27936d478c5c796e8d2ed3ac60ef0550a95f6f1d
Alexi Nasra <alexinasra@gmail.com>
2018-07-03 09:26:01 +0300

    created basic layout for demo

---
+++ demo/Layout/Layout.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+import GridLayout, { GridRow, GridColumn } from '@components/GridLayout';
+
+import AppNav from './AppNav';
+import SideNav from './SideNav';
+
+export default props => (
+  <GridLayout>
+    <GridRow>
+      <GridColumn span="grow">
+        <AppNav />
+      </GridColumn>
+    </GridRow>
+    <GridRow>
+      <GridColumn span={2}>
+        <SideNav />
+      </GridColumn>
+      <GridColumn span="grow">
+        {props.children}
+      </GridColumn>
+    </GridRow>
+  </GridLayout>
+);
