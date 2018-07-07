a60fb37c42f6eca951814db96426dd64ef217795
Alexi Nasra <alexinasra@gmail.com>
2018-07-07 20:23:47 +0300

    added LocaleContext type

--- src/context/LocaleContext.jsx
+++ src/context/LocaleContext.jsx
@@ -1,6 +1,15 @@
 /* @flow */
 import React from 'react';
 
+type LocaleContext = {
+  lng: string,
+  direction: 'ltr' | 'rtl',
+  currency: string,
+  timeFormat: string,
+  dateFormat: string
+};
+export default LocaleContext;
+
 const { Provider, Consumer } = React.createContext({
   lang: 'en',
   direction: 'ltr',
